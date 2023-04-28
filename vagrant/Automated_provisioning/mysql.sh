37e3251c825a4e6e499cc4def0a15d5547c9254e
Hitesh Talhilyani <hiteshtalhilyani@outlook.com>
2023-04-29 00:49:43 +0400

    changed path in scripts

--- vagrant/Automated_provisioning/mysql.sh
+++ vagrant/Automated_provisioning/mysql.sh
@@ -22,7 +22,7 @@ sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
 sudo mysql -u root -p"$DATABASE_PASS" -e "create database accounts"
 sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'localhost' identified by 'admin123'"
 sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'%' identified by 'admin123'"
-sudo mysql -u root -p"$DATABASE_PASS" accounts < /tmp/vprofile-project/src/main/resources/db_backup.sql
+sudo mysql -u root -p"$DATABASE_PASS" accounts < /tmp/Java-WebApp-Local-Setup/project-info/src/main/resources/db_backup.sql
 sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
 
 # Restart mariadb-server
